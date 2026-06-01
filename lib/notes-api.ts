5048b38c32ac5c05930b509ae45258ec6cf1efdc
ernstudy <saingillese@gmail.com>
2026-06-01 16:11:34 -0400

    refactor: clean up code formatting and improve consistency in notes API functions

--- lib/notes-api.ts
+++ lib/notes-api.ts
@@ -1,7 +1,6 @@
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-import type {Note} from '@/lib/types';
-
+import type { Note } from "@/lib/types";
 
 export const getNotesApi = async (token: string) => {
   const response = await fetch(`${apiBaseUrl}/notes`, {
@@ -38,12 +37,7 @@ export const createNewNoteApi = async (token: string, note: Partial<Note>) => {
   return data;
 };
 
-
-
-export const moveNoteToTrashApi = async (
-  token: string,
-  noteId: string,
-) => {
+export const moveNoteToTrashApi = async (token: string, noteId: string) => {
   const response = await fetch(`${apiBaseUrl}/notes/${noteId}/trash`, {
     method: "PATCH",
     headers: {
@@ -60,14 +54,13 @@ export const moveNoteToTrashApi = async (
   return data;
 };
 
-export const removeFromTrashApi = async (token: string, noteId: string) =>{
-  const response = await fetch(`${apiBaseUrl}/notes/${noteId}/untrash`,{
+export const removeFromTrashApi = async (token: string, noteId: string) => {
+  const response = await fetch(`${apiBaseUrl}/notes/${noteId}/untrash`, {
     method: "PATCH",
     headers: {
       Authorization: `Bearer ${token}`,
     },
-
-  })
+  });
 
   if (!response.ok) {
     throw new Error("Failed to remove note from trash");
@@ -76,37 +69,40 @@ export const removeFromTrashApi = async (token: string, noteId: string) =>{
   const data = await response.json();
 
   return data;
-} 
+};
 
-
-export const deleteNotePermenantlyApi = async(token: string, noteId: string) =>{
-  const response = await fetch(`${apiBaseUrl}/notes/${noteId}`,{
+export const deleteNotePermenantlyApi = async (
+  token: string,
+  noteId: string,
+) => {
+  const response = await fetch(`${apiBaseUrl}/notes/${noteId}`, {
     method: "DELETE",
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
+  });
 
   if (!response.ok) {
     throw new Error("Failed to delete note permenantly");
   }
 
   return response.json();
-}
+};
 
-
-
-export const updateNoteApi = async (token: string, note: Partial<Note>, noteId: string) =>{
-
-  const response = await fetch(`${apiBaseUrl}/notes/${noteId}`,{
-    method: "PATCH",
-    headers:{
+export const updateNoteApi = async (
+  token: string,
+  note: Partial<Note>,
+  noteId: string,
+) => {
+  const response = await fetch(`${apiBaseUrl}/notes/${noteId}`, {
+    method: "PUT",
+    headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
 
     body: JSON.stringify(note),
-  })
+  });
 
   if (!response.ok) {
     throw new Error("Failed to update Note");
@@ -115,42 +111,41 @@ export const updateNoteApi = async (token: string, note: Partial<Note>, noteId: 
   const data = await response.json();
 
   return data;
-}
+};
 
-
-export const addNoteToFavoritesApi = async (token: string, noteId: string) =>{
-  const response = await fetch(`${apiBaseUrl}/notes/${noteId}/favorite`,{
+export const addNoteToFavoritesApi = async (token: string, noteId: string) => {
+  const response = await fetch(`${apiBaseUrl}/notes/${noteId}/favorite`, {
     method: "PATCH",
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
+  });
 
-  if(!response.ok){
+  if (!response.ok) {
     throw new Error("Failed to add note to favorites");
   }
 
   const data = await response.json();
 
   return data;
-}
+};
 
-
-export const removeNoteFromFavoritesApi = async (token: string, noteId: string) =>{
-  const response = await fetch(`${apiBaseUrl}/notes/${noteId}/unfavorite`,{
+export const removeNoteFromFavoritesApi = async (
+  token: string,
+  noteId: string,
+) => {
+  const response = await fetch(`${apiBaseUrl}/notes/${noteId}/unfavorite`, {
     method: "PATCH",
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
+  });
 
-  if(!response.ok){
+  if (!response.ok) {
     throw new Error("Failed to remove note from favorites");
   }
 
   const data = await response.json();
 
   return data;
-}
-
-
+};
